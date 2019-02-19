e7435acd264c2b7dd1bb788d61bb1949257d64e1
Janimarton <janimarton22@gmail.com>
2019-02-19 17:55:55 +0100

    in App.js: (line 13) </p> tag added (hot fix)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,18 +10,19 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Learn React
-                        </a>
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
                 </header>
             </div>
-    );
+        );
     }
-    }
+}
 
-    export default App;
+export default App;
