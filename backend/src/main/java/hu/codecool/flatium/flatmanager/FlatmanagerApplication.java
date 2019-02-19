4ce41388986cbf497f3487c54af5f281c3b10ab4
doraKoreny <39567974+doraKoreny@users.noreply.github.com>
2019-02-19 15:25:48 +0100

    initialized spring boot backend

---
+++ backend/src/main/java/hu/codecool/flatium/flatmanager/FlatmanagerApplication.java
@@ -0,0 +1,13 @@
+package hu.codecool.flatium.flatmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatmanagerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/hu/codecool/flatium/flatmanager/FlatmanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.codecool.flatium.flatmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlatmanagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
