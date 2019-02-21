1a66dfa174bbd99d53325241d9efbaef2912fffc
Janimarton <janimarton22@gmail.com>
2019-02-21 12:55:07 +0100

    hotfix & flat Controller request mapping done (something may went wrong before this)

--- backend/src/main/java/hu/codecool/flatium/flatmanager/service/FlatUserStorageService.java
+++ backend/src/main/java/hu/codecool/flatium/flatmanager/service/FlatUserStorageService.java
@@ -31,13 +31,24 @@ public class FlatUserStorageService {
                 .collect(Collectors.toList());
     }
 
-    public void updateFlatUser(FlatUser updatedFlatUser) {
+    public void updateFlatUser(int updateId,FlatUser updatedFlatUser) {
         for (int i = 0; i < flatUserList.size(); i++) {
-            if (flatUserList.get(i).getId() == updatedFlatUser.getId()) {
-                flatUserList.set(i, updatedFlatUser);
+            if (flatUserList.get(i).getId() == updateId) {
+                FlatUser currentFlatUser = flatUserList.get(i);
+                currentFlatUser.setName(updatedFlatUser.getName());
+                currentFlatUser.setEmail(updatedFlatUser.getEmail());
+                currentFlatUser.setPhoneNumber(updatedFlatUser.getPhoneNumber());
+                flatUserList.set(i, currentFlatUser);
             }
         }
     }
 
-
+    public FlatUser getUserById(int id){
+        for (FlatUser flatUser : flatUserList) {
+            if (flatUser.getId() == id) {
+                return flatUser;
+            }
+        }
+        return null;
+    }
 }
