95e6395e9134e431746b92ad22d79a3ab76e26f2
Janimarton <janimarton22@gmail.com>
2019-03-07 10:38:54 +0100

    the comment's flat user id now: required

--- backend/src/main/java/hu/codecool/flatium/flatmanager/model/forum/Comment.java
+++ backend/src/main/java/hu/codecool/flatium/flatmanager/model/forum/Comment.java
@@ -3,6 +3,7 @@ package hu.codecool.flatium.flatmanager.model.forum;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -17,6 +18,7 @@ public class Comment {
     @GeneratedValue
     private int id;
 
+    @Column(nullable = false)
     private int flatUserId;
 
     private String message;
