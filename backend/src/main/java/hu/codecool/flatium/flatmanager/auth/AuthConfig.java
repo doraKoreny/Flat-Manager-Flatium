a90f84c807e48944cc6b1f9a473cc9c5d09b357c
doraKoreny <dorakoreny@gmail.com>
2019-03-20 13:25:42 +0100

    Added authority for landlord and admin

--- backend/src/main/java/hu/codecool/flatium/flatmanager/auth/AuthConfig.java
+++ backend/src/main/java/hu/codecool/flatium/flatmanager/auth/AuthConfig.java
@@ -44,14 +44,32 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
                 .forRS256(apiAudience, issuer)
                 .configure(http)
                 .authorizeRequests()
+
+                .antMatchers(HttpMethod.POST, "/create-flat").hasAuthority("admin")
+                .antMatchers(HttpMethod.DELETE, "/delete-flat").hasAuthority("admin")
+                .antMatchers(HttpMethod.POST, "/add-to-flat").hasAuthority("admin")
+                .antMatchers(HttpMethod.POST, "/create-building").hasAuthority("admin")
+                .antMatchers(HttpMethod.DELETE, "/delete-building").hasAuthority("admin")
+                .antMatchers(HttpMethod.POST, "/add-flat").hasAuthority("admin")
+                .antMatchers(HttpMethod.GET, "/get-comments").hasAuthority("admin")
+
                 .antMatchers(HttpMethod.GET, "/get-myflat/{user_id}").hasAuthority("user")
                 .antMatchers(HttpMethod.POST, "/update-flat").hasAuthority("user")
                 .antMatchers(HttpMethod.POST, "/update-flatuser").hasAuthority("user")
                 .antMatchers(HttpMethod.POST, "/add-comment").hasAuthority("user")
                 .antMatchers(HttpMethod.POST, "/update-comment").hasAuthority("user")
-                .antMatchers(HttpMethod.DELETE, "/delete-comment").hasAuthority("user");
+                .antMatchers(HttpMethod.DELETE, "/delete-comment").hasAuthority("user")
+                .antMatchers(HttpMethod.GET, "/get-comments/{user-id}").hasAuthority("user")
+                .antMatchers(HttpMethod.POST, "/create-flatuser").hasAuthority("user")
+                .antMatchers(HttpMethod.DELETE, "/delete-flatuser").hasAuthority("user")
+                .antMatchers(HttpMethod.POST, "/update-flatuser").hasAuthority("user")
 
-
-
+                .antMatchers(HttpMethod.GET, "/get-flats").hasAuthority("landlord")
+                .antMatchers(HttpMethod.GET, "/get-buildings").hasAuthority("landlord")
+                .antMatchers(HttpMethod.GET, "/get-building").hasAuthority("landlord")
+                .antMatchers(HttpMethod.GET, "/get-flatusers").hasAuthority("landlord")
+                .antMatchers(HttpMethod.POST, "/update-building").hasAuthority("landlord")
+                .antMatchers(HttpMethod.DELETE, "/delete-comment").hasAuthority("landlord");
+        
     }
 }
